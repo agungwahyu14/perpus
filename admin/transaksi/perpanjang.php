1881f735aeac8ebdcb649cfd2fadde0f5b79a4d1
agungwahyu <69448014+agungwahyu14@users.noreply.github.com>
2021-10-14 20:55:05 +0800

    Menambahkan transaksi pada bagian admin dan petugas

--- admin/transaksi/perpanjang.php
+++ admin/transaksi/perpanjang.php
@@ -7,10 +7,10 @@ $tgl_kembali = $_GET['tgl_kembali'];
 $lambat = $_GET['lambat'];
 
 
-if ($lambat > 5) {
+if ($lambat > 2) {
 ?>
 	<script type="text/javascript">
-		alert("buku lewat tidak dapat diperpanjang ");
+		alert("BUKU YANG DI PINJAM TIDAK DAPAT DIPERPANJANG, KARENA TELAH MELEWATI BATAS WAKTU PENGEMBALIAN ");
 		window.location.href = "transaksi.php";
 	</script>
 	<?php
@@ -20,12 +20,12 @@ if ($lambat > 5) {
 	$next_2_hari	= mktime(0, 0, 0, $pecah[1], $pecah[0] + 2, $pecah[2]);
 	$hari_next		= date("d-m-Y", $next_2_hari);
 
-	$update = $conn->query("UPDATE transaksi SET tgl_kembali='$hari_next' where id='$id'");
+	$update = $conn->query("UPDATE tb_transaksi SET tgl_kembali='$hari_next' where id_transaksi='$id'");
 
 	if ($update) {
 	?>
 		<script type="text/javascript">
-			alert("berhasil diperpanjang");
+			alert("BERHASIL DI PERPANJANG");
 			window.location.href = "transaksi.php";
 		</script>
 	<?php
