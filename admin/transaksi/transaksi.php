64bc33f0ce449d4bd28b268955c78af9c94f9cc8
agungwahyu <69448014+agungwahyu14@users.noreply.github.com>
2021-10-15 12:19:21 +0800

    Menambahkan dan memperbaiki transaksi pada admin dan menambahkan fuction terlambat di functions

--- admin/transaksi/transaksi.php
+++ admin/transaksi/transaksi.php
@@ -11,12 +11,12 @@ require '../../functions.php';
 // Pagination
 // konfigurasi
 $jumlahDataPerHalaman = 5;
-$jumlahData = count(query("SELECT * FROM tb_transaksi"));
+$jumlahData = count(query("SELECT * FROM transaksi"));
 $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
 $halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
 $awalData = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
 
-$transaksi = query("SELECT * FROM tb_transaksi WHERE status='pinjam' ORDER BY id_transaksi DESC");
+$transaksi = query("SELECT * FROM transaksi WHERE status='pinjam' ORDER BY id_transaksi DESC");
 
 //Tombol cari diklik
 if (isset($_POST["cari"])) {
@@ -30,8 +30,15 @@ if (isset($_POST["cari"])) {
 <html>
 
 <head>
-  <title>Data Pinjaman Buku</title>
-  <link rel="icon" href="../../icon/logo.png">
+
+
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  <title>Admin Dashboard</title>
+ 
 
   <!-- Bootstrap CSS -->
   <link href="../bs5/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -39,118 +46,196 @@ if (isset($_POST["cari"])) {
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="../../bs5/css/dataTables.bootstrap5.min.css" />
   <link rel="stylesheet" href="../../bs5/css/style.css" />
+  <link href="asset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="asset/css/sb-admin-2.min.css" rel="stylesheet">
 
   <script src="js/jquery.min.js"></script>
 </head>
 
-<body>
-  <!-- top navigation bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
-        <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
-      </button>
-      <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="../home.php">R.Perpustakaan (ADMIN)</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar" aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="topNavBar">
-        <form action="" method="POST" class="d-flex ms-auto my-3 my-lg-0">
-          <div class="input-group">
-            <input class="form-control" id="search" type="text" name="keyword" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-primary" type="submit" name="cari">
-              <i class="bi bi-search"></i>
-            </button>
-          </div>
-        </form>
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <i class="bi bi-person-fill"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><a class="dropdown-item" href="../../logout.php">Logout</a></li>
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-2"> Admin <sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Heading -->
+    <div class="sidebar-heading mt-2 mb-2">
+        <div class="text-center"> Home</div>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active mt-2 mb-2">
+        <a class="nav-link" href="../home.php">
+        <i class="fas fa-fw fa-home"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading mt-2 mb-2">
+        <div class="text-center"> Add User</div>
+       
+        
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="../add/add_siswa.php">
+            <i class="fas fa-fw fa-plus"></i>
+            <span>Add Siswa</span>
+        </a>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="../add/add_petugas.php" >
+            <i class="fas fa-fw fa-plus"></i>
+            <span>Add Petugas</span>
+        </a>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="../add/add_buku.php" >
+            <i class="fas fa-fw fa-plus"></i>
+            <span>Add Buku</span>
+        </a>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="../add/add_admin.php" >
+            <i class="fas fa-fw fa-plus"></i>
+            <span>Add Admin</span>
+        </a>
+    </li>
+
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+    <div class="text-center"> Transaction</div>
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="#" >
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pinjaman</span>
+        </a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item mt-2 mb-2">
+        <a class="nav-link collapsed" href="pengembalian.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Pengembalian</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+</ul>
+<!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!-- Topbar Search -->
+                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="" method="POST">
+                        <div class="input-group">
+                            <input type="text" name="keyword" class="form-control bg-light border-0 small shadow" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" autofocus autocomplete="off">
+                            <div class="input-group-append">
+                                <button name="cari"class="btn btn-primary" type="submit">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+
+
+                <div class="topbar-divider d-none d-sm-block"></div>
+
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-3 d-none d-lg-inline text-gray-600 small">User</span>
+                        <img class="img-profile rounded-circle" src="asset/img/undraw_profile.svg">
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Settings
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Activity Log
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="../../logout.php">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+                    </div>
+                </li>
+
             </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- top navigation bar -->
 
+        </nav>
   <!-- offcanvas -->
-  <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
-    <div class="offcanvas-body p-0">
-      <nav class="navbar-dark">
-        <ul class="navbar-nav">
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3">CORE</div>
-          </li>
-          <li>
-            <a href="../home.php" class="nav-link px-3 active">
-              <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-              <span>Dashboard</span>
-            </a>
-          </li>
-          <li class="my-4">
-            <hr class="dropdown-divider bg-light" />
-          </li>
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">ADD DATA <i class="bi bi-person-plus-fill"></i></div>
-          </li>
-          <li>
-            <a href="../add/add_petugas.php" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-person-lines-fill"></i></span>
-              <span>Petugas</span>
-            </a>
-          </li>
-          <li>
-            <a href="../add/add_siswa.php" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-person-fill"></i></span>
-              <span>Siswa</span>
-            </a>
-          </li>
-          <li>
-            <a href="../add/add_buku.php" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-book-fill"></i></span>
-              <span>Buku</span>
-            </a>
-          </li>
+  <!-- Begin Page Content -->
+          <div class="container-fluid">
 
-          <li class="my-4">
-            <hr class="dropdown-divider bg-light" />
-          </li>
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">Transaksi</div>
-          </li>
-          <li>
-            <a href="transaksi.php" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-arrow-up-right-square"></i></span>
-              <span>Pinjaman</span>
-            </a>
-          </li>
-          <li>
-            <a href="pengembalian.php" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-arrow-down-left-square"></i></span>
-              <span>Pengembalian</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <!-- offcanvas -->
-  <main class="mt-5 pt-3">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card mt-3 shadow mb-4">
-            <div class="card-header bg-primary text-white text-center">
-              Daftar Pinjaman
-            </div>
-            <div class="card-body">
-              <a href="tambah.php" class="btn btn-success mb-3" style="margin-top: 8px;"><i class="bi bi-plus-square"></i> Tambah Pinjaman</a>
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h2 mb-0 text-gray-800">Daftar Pinjaman</h1>
+        </div>
+
+
+
+              <!-- Content Row -->
+              <div class="row">
+              <div class="col-lg-12 mb-4">
+              <div class="card">
+                  <div class="card-header bg-primary text-white"></div>
+              <div class="card-body">
+              
               <br>
               <!-- NAVIGASI PAGINATION -->
               <!-- <?php if ($halamanAktif > 1) : ?>
@@ -168,7 +253,7 @@ if (isset($_POST["cari"])) {
               <?php if ($halamanAktif < $jumlahHalaman) : ?>
                 <a href="?halaman=<?= $halamanAktif + 1; ?>"> &raquo;</a>
               <?php endif; ?> -->
-              <table class="table table-striped table-bordered table-hover text-center mt-3" id="dataTables-example">
+              <table class="table table-bordered table-striped text-center " cellspacing="0" id="dataTables-example">
                 <thead>
                   <tr>
                     <th>No</th>
@@ -218,15 +303,50 @@ if (isset($_POST["cari"])) {
                   <?php endforeach; ?>
                 </tbody>
               </table>
+              </div>
+                 </div>
+
+                        
+                            
+                            </div>
+                          
+
+                        </div>
+                    </div>
+
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
             </div>
-          </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
         </div>
-      </div>
-    </div>
+        <!-- End of Content Wrapper -->
 
     </div>
-    </div>
-  </main>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+
+
+
 
   <script src="../../bs5/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
@@ -252,6 +372,23 @@ if (isset($_POST["cari"])) {
       });
     });
   </script>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="asset/vendor/jquery/jquery.min.js"></script>
+    <script src="asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="asset/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="asset/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="asset/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="asset/js/demo/chart-area-demo.js"></script>
+    <script src="asset/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
